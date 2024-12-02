d6e86f6905da561594a7e1e6862699c94166468f
Ahsan Ali <ahsanbutt6165@gmail.com>
2024-12-02 17:46:03 +0500

    add debug logs

--- server/middlewares/IsAuth.js
+++ server/middlewares/IsAuth.js
@@ -5,12 +5,19 @@ import { User } from "../models/User/UserModel.js";
 export const isAuth = TryCatch(async (req, res, next) => {
   const token = req.cookies.token;
 
+  console.log("================Step 1====================");
+  console.log({ req: req.cookies });
+
   if (!token) return res.status(403).json({ message: "please login" });
+  console.log("=================Step 2===================");
 
   const decodedData = jwt.verify(token, process.env.JWT_SEC);
+  console.log("=================Step 3===================");
   if (!decodedData) return res.status(403).json({ message: "token expired" });
 
+  console.log("=================Step 4===================");
   req.user = await User.findById(decodedData.id);
 
+  console.log("=================Step 5===================");
   next();
 });
